4e372199eb005ed11c54165fbb52e4d0b4b87733
Leandro Arrieta <leandroarrieta@gmail.com>
2021-09-29 21:41:31 -0300

    cambio función en C de Ej 1, 2 y 3
    No utilizo variable auxiliar

--- app/src/c_func.c
+++ app/src/c_func.c
@@ -8,18 +8,32 @@ uint32_t c_sum (uint32_t firstOperand, uint32_t secondOperand)
 
 void zeros (uint32_t * vector, uint32_t longitud)
 {
-	for(uint32_t i = 0 ; i<longitud ; i++ )
-		vector[i]=0;
+	while(--longitud){
+		vector[longitud] = 0;
+	}
+//	for(uint32_t i = 0 ; i<longitud ; i++ )
+//		vector[i]=0;
 }
 
 void c_productoEscalar32 (uint32_t* vectorIn, uint32_t* vectorOut, uint32_t longitud, uint32_t escalar){
 
-	for(uint32_t i = 0 ; i < longitud ; i++){
+	while(--longitud){
+		vectorOut[longitud] = vectorIn[longitud] * escalar;
+	}
+/*
+ 	for(uint32_t i = 0 ; i < longitud ; i++){
 		vectorOut[i] = vectorIn[i] * escalar;
 	}
+*/
 }
 void c_productoEscalar16 (uint16_t* vectorIn, uint16_t* vectorOut, uint32_t longitud, uint16_t escalar){
+
+	while(--longitud){
+			vectorOut[longitud] = vectorIn[longitud] * escalar;
+		}
+/*
 	for(uint32_t i = 0 ; i < longitud ; i++){
 		vectorOut[i] = vectorIn[i] * escalar;
 		}
+*/
 }
