5a7c7ba961f37ff58dd4b1d7d7ab27b944a53ca6
Leandro Arrieta <leandroarrieta@gmail.com>
2021-09-29 21:51:41 -0300

    Se corrige una etiqueta del ejercicio 3 en asm

--- app/src/main.c
+++ app/src/main.c
@@ -82,7 +82,7 @@ static void Producto16 (void)
 {
 	static uint16_t vectorIn[] = { 1, 200, 3, 4, 5, 6, 7, 8, 9, 10};
 	static uint16_t vectorOut[]= { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	static uint16_t escalar = 2;
+	static uint32_t escalar = 2;
 
 	c_productoEscalar32(vectorIn, vectorOut, 10, escalar);
 
